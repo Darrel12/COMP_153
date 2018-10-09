d6b749e667c83b57a324f58da0248ec1b7bdb34e
Darrel Holt <darrelholt1031@gmail.com>
2018-10-08 23:41:01 -0700

    last push was a force due to unidentifiable merge problems. caused system pauses to go away. I put the back in

--- assignment4/Lab7/Lab7/main.cpp
+++ assignment4/Lab7/Lab7/main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])  //main() must take these parameters when using
 	Viewcontroller vc;
 	cout << "created by: Keely Canniff, Darrel Holt, and Norlan Prudente \n";
 	cout << " our 3D model is a skateboard, press the down arrow button to make the wheels spin! \n";
-	//system("pause");
+	system("pause");
 	vc.run();
 
-	//system("pause");
+	system("pause");
 	return 0;
 }
